e318e429ee483e5485d5168b8e6f9cc9fd6aae68
Fredrik <fredrik.jonson@student.kyh.se>
2022-10-05 11:41:14 +0200

    staged

---
+++ WpfApp1/WpfApp1/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private readonly KitchenViewModel _kitchenViewModel;
+
+        public MainViewModel()
+        {
+            _kitchenViewModel = new KitchenViewModel();
+
+            CurrentView = _kitchenViewModel;
+        }
+
+        public ObservableObject CurrentView { get; private set; }
+    }
+}
